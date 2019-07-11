f39e4c6c8ae80e2b5823a9c82be5f45a167d53cc
Gabriel Ferraz <trbesolution@gmail.com>
2019-07-10 19:50:31 -0700

    Created newTaskForm.html

--- src/main/java/com/gf/toDoListMaster/controller/TaskController.java
+++ src/main/java/com/gf/toDoListMaster/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gf.toDoListMaster.model.Task;
 import com.gf.toDoListMaster.service.TaskServiceImpl;
@@ -18,9 +19,25 @@ public class TaskController {
 	TaskServiceImpl taskServiceImpl;
 	
 	@GetMapping("/") 
-	public String getList(Model model) {
+	public String showTaskList(Model model) {
 		List<Task> tasks = taskServiceImpl.findAll();
         model.addAttribute("taskList", tasks);
 		return "toDoList";
 	}
+	
+	@GetMapping("/new") 
+	public String addNewTaskForm(Model model) {
+		model.addAttribute("task", new Task());
+		return "newTaskForm";
+	}
+	
+	@PostMapping("/new")
+	public String submitTaskForm(Task task, Model model) {
+        
+    	taskServiceImpl.save(task);
+        model.addAttribute("successMessage", "Task successfully added!");
+        model.addAttribute("task", new Task());
+        
+        return "newTaskForm";
+    }
 }
